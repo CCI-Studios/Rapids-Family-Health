fe91efe0ba59b8c4c98fdc4b858b12815d79c479
Darren Murray <dmurray@ccistudios.com>
2011-05-09 16:42:24 -0400

    initial site build

---
+++ template/templates/rfht/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<jdoc:include type="head" />
+	<link rel="shortcut icon" href="/templates/rfht/images/favicon.ico" />
+	<link rel="stylesheet" href="/templates/rfht/css/template.css" />
+	
+</head>
+
+
+<body>
+	<div id="top"><div>
+		<jdoc:include type="modules" name="top" style="xhtml" />
+	</div></div>
+	
+	<div id="mainmenu"><div>
+		<jdoc:include type="modules" name="mainmenu" style="xhtml" />
+	</div></div>
+	
+	<div id="header"><div>
+		<jdoc:include type="modules" name="header" style="xhtml" />
+	</div></div>
+	
+	<div id="content"><div>
+	
+	</div></div>
+	
+	<div id="sidebar1"><div>
+		<jdoc:include type="modules" name="sidebar1" style="xhtml" />
+	</div></div>
+	
+	<div id="sidebar2"><div>
+		<jdoc:include type="modules" name="sidebar2" style="xhtml" />
+	</div></div>
+	
+	<div id="bottom"><div><div>
+		<jdoc:include type="modules" name="bottom" style="xhtml" />
+	</div></div></div>
+	
+</body>
+</html>
