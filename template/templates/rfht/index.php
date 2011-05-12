8ef72b78e3bd9eab7fb8132a6c2e343ad5fae85d
Darren Murray <dmurray@ccistudios.com>
2011-05-12 17:28:46 -0400

    final stages of development

--- template/templates/rfht/index.php
+++ template/templates/rfht/index.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
-
+<?php
+$menu = JSite::getMenu();
+if ($menu)
+    $menu = $menu->getActive();
+if ($menu)
+    $menu = $menu->alias;
+?>
 <html>
-
 <head>
 	<jdoc:include type="head" />
 	<link rel="shortcut icon" href="/templates/rfht/images/favicon.ico" />
@@ -10,7 +15,7 @@
 </head>
 
 
-<body>
+<body class="<?php echo $menu ?>">
 	<div id="top"><div>
 		<jdoc:include type="modules" name="top" style="xhtml" />
 		<div class="clear"></div>
@@ -18,17 +23,19 @@
 	
 	<div id="mainmenu"><div>
 		<jdoc:include type="modules" name="mainmenu" style="xhtml" />
+		<div class="clear"></div>
 	</div></div>
 	
 	<div id="header"><div><div><div>
 		<jdoc:include type="modules" name="header" style="xhtml" />
-	</div></div></div></div>
+		<div class="clear"></div>
+	</div></div></div></div></div>
 	
 	<div id="content">
 		
 		<?php
-			$sb1 = $this->countModules('sidebar1');
-			$sb2 = $this->countModules('sidebar2');
+			$sb1 = $this->countModules('sidebar_right');
+			$sb2 = $this->countModules('sidebar_left');
 			
 			if ($sb1 !== 0 && $sb2 !== 0) {
 				$width = 'class="narrow"';
@@ -38,32 +45,37 @@
 				$width = '';
 			}
 		?>
-		<div id="component" <?php echo $width; ?>>
+		<div id="component" <?php echo $width; ?>><div>
 			<jdoc:include type="component" />
-		</div>
+		</div></div>
 		
-		<?php if ($this->countModules('sidebar1')): ?>
-		<div id="sidebar1"><div>
-			<jdoc:include type="modules" name="sidebar1" style="xhtml" />
+		<?php if ($this->countModules('sidebar_left')): ?><div>
+		<div id="sidebar_left"><div>
+			<jdoc:include type="modules" name="sidebar_left" style="xhtml" />
+			<div class="clear"></div>
+		</div></div></div>
+		<?php endif; ?>
+		
+		<?php if ($this->countModules('sidebar_right')): ?>
+		<div id="sidebar_right"><div>
+			<jdoc:include type="modules" name="sidebar_right" style="xhtml" />
+			<div class="clear"></div>
 		</div></div>
 		<?php endif; ?>
 		
-		<?php if ($this->countModules('sidebar2')): ?>
-		<div id="sidebar2"><div>
-			<jdoc:include type="modules" name="sidebar2" style="xhtml" />
-		</div></div>
-		<?php endif; ?>
 		<div class="clear"></div>
 	</div>
 	
-	<div id="bottom">
-		<div><jdoc:include type="modules" name="bottom" style="xhtml" /></div>
-		<div><div>
-			<div class="left">&copy; Rapids Family Health Team <?php echo date ('Y')?>. All Rights Reserved.
-			<div class="right">Site by<a href="http://www.ccistudios.com" target="_blank"> CCI Studios</a>
-		</div></div>
-		</div></div>	
-	</div>
+	<div id="bottom"><div>
+		<div>
+			<jdoc:include type="modules" name="bottom" style="xhtml" />
+			<div class="clear"></div>
+		</div>
+		<div>
+			<div class="left">&copy; Rapids Family Health Team <?php echo date ('Y')?>. All Rights Reserved.</div>
+			<div class="right">Site by<a href="http://www.ccistudios.com" target="_blank"> CCI Studios</a></div>
+		</div>	
+	</div></div>
 	
 </body>
 </html>
