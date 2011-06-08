ebce14440a1b59b54387ace4b479d461a30e6319
Darren Murray <dmurray@ccistudios.com>
2011-06-08 16:53:42 -0400

    removed a blank line

--- template/templates/rfht/index.php
+++ template/templates/rfht/index.php
@@ -84,6 +84,18 @@ echo JHtml::_('behavior.modal');
 		<jdoc:include type="modules" name="hidden" style="xhtml" />
 		<div class="clear"></div>
 	</div>
-	
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-23721706-1']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
 </body>
 </html>
